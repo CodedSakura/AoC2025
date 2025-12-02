41db55cb3f92db4cb80470c3b4ce94ba4154424b
CodedSakura <me@codedsakura.dev>
2025-12-02 22:22:18 +0200

    Day 01 - improve B

--- 01/B.c
+++ 01/B.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 8009 - too high
-// 2774 - too low
-// 6819 - right, but gotten with brute force
 int main() {
     FILE* file_handle = fopen("input.txt", "r");
     if (file_handle == NULL) {
@@ -24,20 +21,12 @@ int main() {
             line_index = 0;
 
             const char direction = line[0];
-            const char sign = direction == 'L' ? -1 : 1;
-            const int amount = atoi(&line[1]);
-            // printf("%+2d", direction, amount, dial_position);
-            // times_pointed_at_0 += direction == 'L' && amount > dial_position;
-            // times_pointed_at_0 += direction == 'R' && 100 - amount <= dial_position;
-            for (int i = 0; i < amount; i++) {
-                dial_position = (dial_position + sign + 100) % 100;
-                if (dial_position == 0) {
-                    times_pointed_at_0++;
-                    // printf("%s %d\n", line, times_pointed_at_0);
-                }
-            }
-            // dial_position += amount;
-            // printf(" -> %+4d (%2d) %d\n", dial_position, (dial_position + 100) % 100, times_pointed_at_0);
+            int amount = atoi(&line[1]);
+            times_pointed_at_0 += amount / 100;
+            amount %= 100;
+            times_pointed_at_0 += direction == 'L' && amount > dial_position;
+            times_pointed_at_0 += direction == 'R' && amount + dial_position >= 100;
+            dial_position += direction == 'L' ? -amount : amount;
             dial_position = (dial_position + 100) % 100;
         }
     }
